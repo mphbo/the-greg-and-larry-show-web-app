f2cd3dd1ca359c8124546d021eda53cb5c3e4952
logan.thomas <logan.thomas@forsta.com>
2022-09-05 20:06:53 -0700

    began work on the registration and login form, installed material ui

---
+++ components/LoginForm.tsx
@@ -0,0 +1,42 @@
+import { Button, createTheme, Grid, TextField } from "@mui/material";
+import { Container } from "@mui/system";
+
+export const LoginForm = (props: { setIsRegistration: () => void }) => {
+  const { setIsRegistration } = props;
+  return (
+    <Container maxWidth="md">
+      <Grid container xs={12} spacing={2}>
+        <Grid item xs={6}>
+          <TextField label="First name" />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField label="Last name" />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField label="Email" />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField label="Password" />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField label="Re-enter password" />
+        </Grid>
+        <Grid item xs={6}>
+          <Button variant="contained" fullWidth>
+            Register
+          </Button>
+        </Grid>
+        <Grid item xs={6}>
+          <Button color="warning" variant="contained" fullWidth>
+            Continue without saving
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" color="secondary" fullWidth>
+            Already have an account? Login
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
